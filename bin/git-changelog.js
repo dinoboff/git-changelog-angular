df893fea683da64aeeb89de444d2a27396ec4aae
Damien Lebrun <dinoboff@hotmail.com>
2017-02-06 13:28:41 +0000

    feat: support implicit range
    The providing a revision to create a change between that revision and the previous tag.

--- bin/git-changelog.js
+++ bin/git-changelog.js
@@ -5,12 +5,10 @@
 const changelog = require('../');
 const cli = require('../lib/cli');
 
-const config = cli.config();
-
-changelog.create(config)
+cli.config()
+  .then(config => changelog.create(config))
   .then(console.log)
   .catch(err => {
     console.error(err.stack);
-    config.showHelp();
     process.exit(1);
   });
--- test/cli/config.js
+++ test/cli/config.js
@@ -8,36 +8,70 @@ const repo = tempRepo();
 
 test.before(async () => {
   await repo.init();
+  await repo.commit('chore: chore 1');
+  await repo.commit('chore: chore 2');
+  await repo.git('tag', 'v0.0.0');
+  await repo.commit('feat: feat1');
+  await repo.commit('feat: feat2');
+  await repo.git('tag', 'v0.1.0-0');
+  await repo.commit('fix: fix1');
+
+  await repo.git('checkout', '-b', 'stable');
+  await repo.commit('fix: fix2');
+  await repo.git('tag', 'v0.1.0');
+
+  await repo.git('checkout', 'master');
 });
 
 test.after.always(async () => {
   await repo.remove();
 });
-test('default config', matchConfig, [], {
-  rev: ['HEAD'],
-  gitDir: undefined
+
+test('default config', matchConfig, ['--git-dir', repo.gitDir], {
+  rev: ['HEAD', '^v0.0.0'],
+  gitDir: repo.gitDir
 });
 
-test('config with revision list', matchConfig, ['^foo', 'bar'], {
+test('config with "--any" flag', matchConfig, ['--any', '--git-dir', repo.gitDir], {
+  rev: ['HEAD', '^v0.1.0-0'],
+  gitDir: repo.gitDir
+});
+
+test('config with revision', matchConfig, ['--git-dir', repo.gitDir, 'master'], {
+  rev: ['master', '^v0.0.0'],
+  gitDir: repo.gitDir
+});
+
+test('config with tagged revision', matchConfig, ['--git-dir', repo.gitDir, 'stable'], {
+  rev: ['stable', '^v0.0.0'],
+  gitDir: repo.gitDir
+});
+
+test('config with revision range', matchConfig, ['^foo', 'bar'], {
   rev: ['^foo', 'bar'],
   gitDir: undefined
 });
 
-test('config with --git-dir', matchConfig, ['--git-dir', './.git'], {
-  rev: ['HEAD'],
+test('config with dotted revision range', matchConfig, ['foo..bar'], {
+  rev: ['foo..bar'],
+  gitDir: undefined
+});
+
+test('config with "--git-dir" flag', matchConfig, ['--git-dir', './.git', '^foo', 'bar'], {
+  rev: ['^foo', 'bar'],
   gitDir: '.git'
 });
 
 test('config client with --git-dir', async t => {
-  const config = cli.config(['--git-dir', repo.gitDir]);
+  const config = await cli.config(['--git-dir', repo.gitDir]);
   const gitDir = await config.client(['rev-parse', '--git-dir'], {capture: true});
 
   t.is(gitDir.trim(), repo.gitDir);
 });
 
-function matchConfig(t, args, expected) {
+async function matchConfig(t, args, expected) {
   const fns = ['client', 'showHelp'];
-  const config = cli.config(args);
+  const config = await cli.config(args);
 
   t.deepEqual(omit(config, fns), omit(expected, fns));
   t.is(typeof config.client, 'function');
