5d9e324a3d2338f04f1146f06a6c634d02b816a0
Damien Lebrun <dinoboff@hotmail.com>
2017-02-09 20:02:48 +0000

    chore(build): exit early on error

--- tools/bin/release.sh
+++ tools/bin/release.sh
@@ -21,18 +21,32 @@ echo
 git changelog HEAD > "$FILE"
 cat "$FILE"
 
-echo "Bumping package version..."
+
 VERSION=$(head -n 1 "$FILE")
-npm version "$VERSION"
+
+echo "Bumping package version..."
+if npm version "$VERSION"; then
+    echo "Version set to ${VERSION}."
+else
+    echo "ERROR: failed to update version to ${VERSION}!"
+    exit 1
+fi
+
 
 echo "Pushing changes..."
-git push --tags
+if git push && git push --tags; then
+    echo "New tag sent to origin"
+else
+    echo "ERROR: Failed to update origin!"
+    exit 1
+fi
 
 
-
-echo "Checking ci status..."
 TAG="v${VERSION}"
 REV="${TAG}^{commit}"
+
+echo "Checking ci status..."
+
 hub ci-status -v "$REV"
 while [ $? -ne 0 ]; do
     echo 'retrying in 10s...'
@@ -40,18 +54,22 @@ while [ $? -ne 0 ]; do
     hub ci-status "$REV"
 done
 
+
 echo
 read -r -p "Publish package? [y/N] " response
 
 case "$response" in
+
     [yY][eE][sS]|[yY])
         npm publish
         hub release create --draft --file="$FILE" --browse "$TAG"
         ;;
+
     *)
         echo
         echo "HEAD and tags still need  to be updated."
         echo "${TAG} is not yet published to npm."
         echo
         ;;
+
 esac
